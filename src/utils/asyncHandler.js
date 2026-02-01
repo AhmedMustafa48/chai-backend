8d9217076cf1fff42c58b97f3e3f33c9a04b245e
AhmedMustafa48 <ahmedcodes48@gmail.com>
2026-02-01 12:35:51 +0500

    feat: implement ApiError class and asyncHandler utility

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+const asyncHandler = (requestHandler) =>{
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch(err => next(err));
+    }
+}
+
+export { asyncHandler };
+
+// higher order function because fn is a function which is passed as argument
+
+// const asyncHandler = () => {};
+// const asyncHandler = () => () => {};
+// xconst asyncHandler = () => async() => {};
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message || "Internal Server Error",
+//     });
+//   }
+// };
+
+
+// this above function is in top with promises
