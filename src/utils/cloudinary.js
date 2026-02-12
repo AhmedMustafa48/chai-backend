1e1d9e6a25f5289dc6e71af8b4dd75bc6cd61278
AhmedMustafa48 <ahmedcodes48@gmail.com>
2026-02-12 07:57:11 +0500

    feat: enhance user registration by improving cover image handling and cleanup after upload

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -26,7 +26,8 @@ const uploadOnCloudinary = async (localFilePath) => {
       resource_type: "auto",
     });
     // file has been uploaded successfully
-    console.log("file is uploaded on cloudinary", response.url);
+    // console.log("file is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     console.error("Error uploading to Cloudinary:", error.message);
