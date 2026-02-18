bf4f504a573b3b35224b1ddf2fb438636f631f0f
AhmedMustafa48 <ahmedcodes48@gmail.com>
2026-02-18 12:49:23 +0500

    feat: enhance refresh token functionality with error handling and token generation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -168,28 +168,44 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   if (!incomingRefreshToken) {
     throw new ApiError(401, "Unauthorized request");
   }
-  // contiue from herea
-  const decodedToken = jwt.verify(
-    incomingRefreshToken,
-    process.env.REFRESH_TOKEN_SECRET
-  );
 
-  const user = await User.findById(decodedToken?._id);
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
 
-  if (!user) {
-    throw new ApiError(401, "Invalid refresh token");
+    const user = await User.findById(decodedToken?._id);
+
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token");
+    }
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Refresh token is expired or used");
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
+      user._id
+    );
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", refreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          "Access token refreshed successfully"
+        )
+      );
+  } catch (error) {
+    throw new ApiError(401, error.message || "Invalid refresh token");
   }
-  if (incomingRefreshToken !== user?.refreshToken) {
-    throw new ApiError(401, "Refresh token is expired or used");
-  }
-
-  const options = {
-    httpOnly: true,
-    secure: true,
-  };
-  await generateAccessAndRefreshTokens(user._id);
-
-  return res.status(200).cookie("accessToken").cookie("refreshToken")
 });
 
 export { registerUser, loginUser, logoutUser };
