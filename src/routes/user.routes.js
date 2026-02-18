66100b27ea205a4addcc2925b38785b12052851c
AhmedMustafa48 <ahmedcodes48@gmail.com>
2026-02-18 12:54:16 +0500

    feat: add refresh token route to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -22,5 +22,6 @@ router.route("/login").post(loginUser);
 // Secured route
 
 router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
